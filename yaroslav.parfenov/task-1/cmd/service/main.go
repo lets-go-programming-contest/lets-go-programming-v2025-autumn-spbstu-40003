2723c5cc1e843f856a53fb1db03156c8d3a8336d
Yaroslav Parfenov <mailpost336@gmail.com>
2025-09-19 19:16:08 +0300

    [TASK-1] Initial commit for task-1

---
+++ yaroslav.parfenov/task-1/cmd/service/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "FirstProject/internal"
+
+func main() {
+	internal.Calculate()
+}
---
+++ yaroslav.parfenov/task-1/internal/calculate.go
@@ -0,0 +1,30 @@
+package internal
+
+import "fmt"
+
+func Calculate() {
+	var (
+		firOperand int
+		secOperand int
+		operation  byte
+		err        error
+	)
+
+	err = InputOperandsAndOperation(&firOperand, &secOperand, &operation)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		switch operation {
+		case '+':
+			fmt.Println(float64(firOperand + secOperand))
+		case '-':
+			fmt.Println(float64(firOperand - secOperand))
+		case '*':
+			fmt.Println(float64(firOperand * secOperand))
+		case '/':
+			fmt.Println(float64(firOperand / secOperand))
+		}
+	}
+
+}
