3455f3d57c4173a58b92bceae0edaea0e24d886a
kirinnah <inna.kirillova005@gmail.com>
2025-09-21 01:54:32 +0300

    [TASK-1] Initial commit

---
+++ inna.kirillova/task-1/cmd/service/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	var operation string
+
+	_, errorOfInvalidFirstOperand := fmt.Scanln(&a)
+    if errorOfInvalidFirstOperand != nil {
+        fmt.Println("Invalid first operand")
+        return}
+
+	_, errorOfInvalidSecondOperand := fmt.Scanln(&b)
+    if errorOfInvalidSecondOperand != nil {
+        fmt.Println("Invalid second operand")
+        return
+    }
+
+	_, errorOfInvalidOperation := fmt.Scanln(&operation)
+    if errorOfInvalidOperation != nil {
+        fmt.Println("Invalid operation")
+        return
+    }
+
+	switch operation {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+		} else {
+			fmt.Println(a / b)
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
