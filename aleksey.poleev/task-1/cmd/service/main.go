864a8be989958f910dc810232aaaed20605ff1a4
whyEnot13 <pol13ev@yandex.ru>
2025-09-21 20:00:14 +0300

    [TASK-1] lint fix

--- aleksey.poleev/task-1/cmd/service/main.go
+++ aleksey.poleev/task-1/cmd/service/main.go
@@ -4,39 +4,39 @@ import "fmt"
 
 func main() {
 	var (
-		op   string
-		a, b int
+		operation  string
+		num1, num2 int
 	)
 
-	_, err := fmt.Scan(&a)
+	_, err := fmt.Scan(&num1)
 	if err != nil {
 		fmt.Println("Invalid first operand")
 		return
 	}
-	_, err = fmt.Scan(&b)
+	_, err = fmt.Scan(&num2)
 	if err != nil {
 		fmt.Println("Invalid second operand")
 		return
 	}
-	_, err = fmt.Scan(&op)
+	_, err = fmt.Scan(&operation)
 	if err != nil {
 		fmt.Println("Operation input error")
 		return
 	}
 
-	switch op {
+	switch operation {
 	case "+":
-		fmt.Println(a + b)
+		fmt.Println(num1 + num2)
 	case "-":
-		fmt.Println(a - b)
+		fmt.Println(num1 - num2)
 	case "*":
-		fmt.Println(a * b)
+		fmt.Println(num1 * num2)
 	case "/":
-		if b == 0 {
+		if num2 == 0 {
 			fmt.Println("Division by zero")
 			return
 		}
-		fmt.Println(a / b)
+		fmt.Println(num1 / num2)
 	default:
 		fmt.Println("Invalid operation")
 		return
