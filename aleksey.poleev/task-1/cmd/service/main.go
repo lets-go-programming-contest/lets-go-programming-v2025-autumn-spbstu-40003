cc751625434067a5ccdb6ef94cd47b69abebce7b
whyEnot13 <pol13ev@yandex.ru>
2025-09-21 19:40:37 +0300

    [TASK-1]

---
+++ aleksey.poleev/task-1/cmd/service/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+func main() {
+	var (
+		op   string
+		a, b int
+	)
+
+	_, err := fmt.Scan(&a)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&b)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&op)
+	if err != nil {
+		fmt.Println("Operation input error")
+		return
+	}
+
+	switch op {
+	case "+":
+		fmt.Println(a + b)
+	case "-":
+		fmt.Println(a - b)
+	case "*":
+		fmt.Println(a * b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(a / b)
+	default:
+		fmt.Println("Invalid operation")
+		return
+	}
+}
