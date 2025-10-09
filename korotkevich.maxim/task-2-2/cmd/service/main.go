f02d59c0769529b6e27724f1debfd3c769cdcbef
Maxim <maximkor03@mail.ru>
2025-10-09 17:35:59 +0300

    [TASK-2-2] Init commit

---
+++ korotkevich.maxim/task-2-2/cmd/service/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"errors"
+)
+
+var (
+	ErrInvalidDish       = errors.New("incorrect input count of dish")
+	ErrInvalidValue      = errors.New("incorrect dish value")
+	ErrInvalidPreference = errors.New("incorrect preference of dish value")
+)
+
+type IntHeap []int
+
+func (heap *IntHeap) Len() int {
+	return len(*heap)
+}
+
+func (heap *IntHeap) Less(i, j int) bool {
+	return (*heap)[i] > (*heap)[j]
+}
+
+func (heap *IntHeap) Swap(i, j int) {
+	(*heap)[i], (*heap)[j] = (*heap)[j], (*heap)[i]
+}
+
+func (heap *IntHeap) Push(value interface{}) {
+	if intValue, ok := value.(int); ok {
+		*heap = append(*heap, intValue)
+	}
+}
+
+func (heap *IntHeap) Pop() interface{} {
+	currentHeap := *heap
+	length := len(currentHeap)
+	lastElement := currentHeap[length-1]
+	*heap = currentHeap[0 : length-1]
+
+	return lastElement
+}
+
+func main() {
+	var (
+		countOfDish      int
+		preferenceOfDish int
+	)
+
+	const (
+		MinValue  = -10000
+		MinDishes = 1
+		MaxValue  = 10000
+	)
+
+	_, err := fmt.Scan(&countOfDish)
+	if err != nil || countOfDish < MinDishes || countOfDish > MaxValue {
+		fmt.Println(ErrInvalidDish)
+
+		return
+	}
+
+	h := &IntHeap{}
+	heap.Init(h)
+
+	for i := 0; i < countOfDish; i++ {
+		var value int
+		_, err = fmt.Scan(&value)
+		if err != nil || value < MinValue || value > MaxValue {
+			fmt.Println(ErrInvalidValue)
+
+			return
+		}
+		heap.Push(h, value)
+	}
+	
+	_, err = fmt.Scan(&preferenceOfDish)
+	if err != nil || preferenceOfDish < 1 || preferenceOfDish > countOfDish {
+		fmt.Println(ErrInvalidPreference)
+		return
+	}
+
+	for preferenceOfDish > 0 {
+		result := heap.Pop(h).(int)
+		preferenceOfDish--
+		if preferenceOfDish == 0 {
+			fmt.Println(result)
+		}
+	}
+}
