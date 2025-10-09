d204547c8c8a3a0a8a91cd1299f616e4ab3f1e01
Artem-Hack <cacinartem1@gmail.com>
2025-10-10 01:01:29 +0300

    [TASK-2-1] Init commit

---
+++ artyom.tsatsin/task-2-1/cmd/service/main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrDep  = errors.New("incorrect number of departments")
+	ErrEmp  = errors.New("incorrect number of employees")
+	ErrTemp = errors.New("invalid temperature")
+	ErrSign = errors.New("invalid sign")
+)
+
+const (
+	minT     = 15
+	maxT     = 30
+	minV     = 1
+	maxV     = 1000
+	invalidT = -1
+)
+
+func main() {
+	var depCount int
+
+	_, err := fmt.Scan(&depCount)
+	if err != nil {
+		fmt.Println(ErrDep)
+		return
+	}
+
+	if depCount < minV || depCount > maxV {
+		fmt.Println(invalidT)
+		return
+	}
+
+	for range loopCount(depCount) {
+		var empCount int
+
+		_, err := fmt.Scan(&empCount)
+		if err != nil {
+			fmt.Println(invalidT)
+			continue
+		}
+
+		if empCount < minV || empCount > maxV {
+			fmt.Println(invalidT)
+			continue
+		}
+
+		err = processDepartment(empCount)
+		if err != nil {
+			fmt.Println(invalidT)
+		}
+	}
+}
+
+func processDepartment(empCount int) error {
+	if empCount < minV || empCount > maxV {
+		return ErrEmp
+	}
+
+	depLow := minT
+	depHigh := maxT
+	valid := true
+
+	for range loopCount(empCount) {
+		var sign string
+		var temp int
+
+		_, err := fmt.Scan(&sign, &temp)
+		if err != nil {
+			return ErrTemp
+		}
+
+		if temp < minT || temp > maxT {
+			return ErrTemp
+		}
+
+		if !valid {
+			continue
+		}
+
+		switch sign {
+		case "<=":
+			depHigh = min(depHigh, temp)
+		case ">=":
+			depLow = max(depLow, temp)
+		default:
+			return ErrSign
+		}
+
+		if depLow > depHigh {
+			valid = false
+		}
+	}
+
+	if valid {
+		fmt.Println(depLow)
+	} else {
+		fmt.Println(invalidT)
+	}
+
+	return nil
+}
+
+func loopCount(total int) []struct{} {
+	return make([]struct{}, total)
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
