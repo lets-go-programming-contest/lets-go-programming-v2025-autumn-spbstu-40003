2ab9cb280aba2e8883ba3a94641a11ff4b2ac130
Edwin Denisov-Elers <edwindenisovelers9864@gmail.com>
2025-10-09 21:37:34 +0300

    [TASK-3] fix create output file

--- edwin.denisovelers/task-3/internal/utils/save.go
+++ edwin.denisovelers/task-3/internal/utils/save.go
@@ -14,6 +14,11 @@ func Save(valutes []JSONValute, path string) error {
 
 	const permissionCode = 0o0644
 
+	_, err = os.Create(path)
+	if err != nil {
+		return fmt.Errorf("json file create: %w", err)
+	}
+
 	err = os.WriteFile(path, valutesJSON, permissionCode)
 	if err != nil {
 		return fmt.Errorf("json write: %w", err)
