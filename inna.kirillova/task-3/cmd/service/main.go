406b11fd214a31405451b15eb818b15a281766b7
kirinnah <inna.kirillova005@gmail.com>
2025-10-16 22:35:11 +0300

    [TASK-3] Initial commit

---
+++ inna.kirillova/task-3/cmd/service/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/kirinnah/task-3/internal/exchangetrade"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "path to configuration file")
+	flag.Parse()
+
+	exchangetrade.Process(*configPath)
+}
+
---
+++ inna.kirillova/task-3/internal/exchangetrade/jsonwriter.go
@@ -0,0 +1,51 @@
+package exchangetrade
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func saveJSON(path string, data []ExchangeTrade) {
+	const dirPerm = 0o755
+
+	if len(data) == 0 {
+		panic("error: no data to save to JSON file")
+	}
+
+	dir := filepath.Dir(path)
+
+	if err := os.MkdirAll(dir, dirPerm); err != nil {
+		panic("error while creating output directory: " + err.Error())
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		panic("error while creating output file: " + err.Error())
+	}
+
+	defer func() {
+		if err := file.Close(); err != nil {
+			panic("error while closing file: " + err.Error())
+		}
+	}()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(data); err != nil {
+		panic("error while encoding data to JSON: " + err.Error())
+	}
+
+	fmt.Printf("successfully saved %d exchange trades to %s\n", len(data), path)
+}
+
+func Process(configPath string) {
+	config := loadConfig(configPath)
+	trades := parseXML(config.InputFile)
+	saveJSON(config.OutputFile, trades)
+
+	fmt.Printf("processing completed: %d exchange trades sorted and saved\n", len(trades))
+}
+
