c86df8f7f657d1d84e48cb38ad5d82e5539cc493
kirinnah <inna.kirillova005@gmail.com>
2025-10-16 18:56:14 +0300

    [TASK-3] Fix lint again x8

--- inna.kirillova/task-3/internal/exchangetrade/xmlparser.go
+++ inna.kirillova/task-3/internal/exchangetrade/xmlparser.go
@@ -1,69 +1,69 @@
 package exchangetrade
 
 import (
-	"encoding/xml"
-	"os"
-	"sort"
-	"strconv"
-	"strings"
+"encoding/xml"
+"os"
+"sort"
+"strconv"
+"strings"
 
-	"golang.org/x/net/html/charset"
+"golang.org/x/net/html/charset"
 )
 
 type ExchangeTrade struct {
-	NumCode  int     `json:"num_code"`
-	CharCode string  `json:"char_code"`
-	Value    float64 `json:"value"`
+NumCode  int     `json:"num_code"`
+CharCode string  `json:"char_code"`
+Value    float64 `json:"value"`
 }
 
 type xmlData struct {
-	Items []struct {
-		NumCode  string `xml:"NumCode"`
-		CharCode string `xml:"CharCode"`
-		Value    string `xml:"Value"`
-	} `xml:"Valute"`
+Items []struct {
+NumCode  string `xml:"NumCode"`
+CharCode string `xml:"CharCode"`
+Value    string `xml:"Value"`
+} `xml:"Valute"`
 }
 
 func parseXML(path string) []ExchangeTrade {
-	file, err := os.Open(path)
-	if err != nil {
-		panic("XML error: " + err.Error())
-	}
+file, err := os.Open(path)
+if err != nil {
+panic("XML error: " + err.Error())
+}
 
-	defer func() {
-		if err := file.Close(); err != nil {
-			panic("error while closing file: " + err.Error())
-		}
-	}()
+defer func() {
+if err := file.Close(); err != nil {
+panic("error while closing file: " + err.Error())
+}
+}()
 
-	decoder := xml.NewDecoder(file)
-	decoder.CharsetReader = charset.NewReaderLabel
+decoder := xml.NewDecoder(file)
+decoder.CharsetReader = charset.NewReaderLabel
 
-	var data xmlData
-	if err := decoder.Decode(&data); err != nil {
-		panic("bad XML: " + err.Error())
-	}
+var data xmlData
+if err := decoder.Decode(&data); err != nil {
+panic("bad XML: " + err.Error())
+}
 
-	trades := make([]ExchangeTrade, 0, len(data.Items))
+trades := make([]ExchangeTrade, 0, len(data.Items))
 
-	for _, item := range data.Items {
-		numCode, _ := strconv.Atoi(strings.TrimSpace(item.NumCode))
-		value, err := strconv.ParseFloat(strings.ReplaceAll(item.Value, ",", "."), 64)
+for _, item := range data.Items {
+numCode, _ := strconv.Atoi(strings.TrimSpace(item.NumCode))
+value, err := strconv.ParseFloat(strings.ReplaceAll(item.Value, ",", "."), 64)
 
-		if err != nil {
-			continue
-		}
+if err != nil {
+continue
+}
 
-		trades = append(trades, ExchangeTrade{
-			NumCode:  numCode,
-			CharCode: strings.TrimSpace(item.CharCode),
-			Value:    value,
-		})
-	}
+trades = append(trades, ExchangeTrade{
+NumCode:  numCode,
+CharCode: strings.TrimSpace(item.CharCode),
+Value:    value,
+})
+}
 
-	sort.Slice(trades, func(i, j int) bool {
-		return trades[i].Value > trades[j].Value
-	})
+sort.Slice(trades, func(i, j int) bool {
+return trades[i].Value > trades[j].Value
+})
 
-	return trades
+return trades
 }
