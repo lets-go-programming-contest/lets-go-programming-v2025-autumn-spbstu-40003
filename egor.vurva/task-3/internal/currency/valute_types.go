f84de855cc488957c5a727f02c3817ec23bb639a
Vurvaa <egor.vurva@gmail.com>
2025-10-16 19:46:29 +0300

    [TASK-3] fix: normalize formatting

--- egor.vurva/task-3/internal/currency/valute_types.go
+++ egor.vurva/task-3/internal/currency/valute_types.go
@@ -1,6 +1,5 @@
 package currency
 
-type Value64 float64
 type Config struct {
 	InputFile  string `yaml:"input-file"`
 	OutputFile string `yaml:"output-file"`
@@ -12,12 +11,15 @@ type ValCurs struct {
 	Valutes []Valute `xml:"Valute"`
 }
 
-type Valute struct {
-	ID        string  `json:"-"         xml:"ID"`
-	NumCode   int     `json:"num_code"  xml:"NumCode"`
-	CharCode  string  `json:"char_code" xml:"CharCode"`
-	Nominal   int     `json:"-"         xml:"Nominal"`
-	Name      string  `json:"-"         xml:"Name"`
-	Value     Value64 `json:"value"     xml:"Value"`
-	VunitRate string  `json:"-"         xml:"VunitRate"`
-}
+type (
+	Value64 float64
+	Valute  struct {
+		ID        string  `json:"-"         xml:"ID"`
+		NumCode   int     `json:"num_code"  xml:"NumCode"`
+		CharCode  string  `json:"char_code" xml:"CharCode"`
+		Nominal   int     `json:"-"         xml:"Nominal"`
+		Name      string  `json:"-"         xml:"Name"`
+		Value     Value64 `json:"value"     xml:"Value"`
+		VunitRate string  `json:"-"         xml:"VunitRate"`
+	}
+)
