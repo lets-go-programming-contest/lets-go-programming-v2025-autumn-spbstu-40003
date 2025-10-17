65e14a0f4bce74418609f3d8a8e26632499d56c8
Fedorov Vyacheslav <fortochkamcs@gmail.com>
2025-10-17 19:03:13 +0300

    [TASK-2-2] Initial commit

---
+++ vyacheslav.fedorov/task-2-2/cmd/service/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrorItRat  = errors.New("invalid rating value")
+	ErrorSel    = errors.New("invalid k value")
+	ErroriCount = errors.New("invalid number of menu items")
+)
+
+const (
+	lRat  = -10000
+	hRat  = 10000
+	minIt = 1
+)
+
+type Score []int
+
+func (d *Score) Push(x interface{}) {
+	if val, ok := x.(int); ok {
+		*d = append(*d, val)
+	}
+}
+
+func (d *Score) Swap(i, j int) {
+	(*d)[i], (*d)[j] = (*d)[j], (*d)[i]
+}
+
+func (d *Score) Less(i, j int) bool {
+	return (*d)[i] > (*d)[j]
+}
+
+func (d *Score) Len() int {
+	return len(*d)
+}
+
+func (d *Score) Pop() interface{} {
+	current := *d
+	n := len(current)
+	elem := current[n-1]
+	*d = current[:n-1]
+
+	return elem
+}
+
+func main() {
+	var (
+		iCount    int
+		selection int
+	)
+
+	_, scanErr1 := fmt.Scan(&iCount)
+	failedCountCheck := scanErr1 != nil || iCount < minIt || iCount > hRat
+	if failedCountCheck {
+		fmt.Println(ErroriCount)
+
+		return
+	}
+
+	mData, err := collectScores(iCount, lRat, hRat)
+	if err != nil {
+		fmt.Println(err)
+
+		return
+	}
+
+	_, scanErr2 := fmt.Scan(&selection)
+	failedSelectionCheck := scanErr2 != nil || selection < 1 || selection > iCount
+	if failedSelectionCheck {
+		fmt.Println(ErrorSel)
+
+		return
+	}
+
+	remaining := selection
+	for {
+		if remaining <= 0 {
+			break
+		}
+		result, Tr := heap.Pop(mData).(int)
+		if !Tr {
+			fmt.Println(-1)
+
+			return
+		}
+
+		remaining--
+		if remaining == 0 {
+			fmt.Println(result)
+		}
+	}
+}
+
+func collectScores(count, minB, maxB int) (*Score, error) {
+	data := &Score{}
+	heap.Init(data)
+
+	for i := 0; i < count; i++ {
+		var score int
+		_, scanErr := fmt.Scan(&score)
+
+		if scanErr != nil || score < minB || score > maxB {
+			return nil, ErrorItRat
+		}
+
+		heap.Push(data, score)
+	}
+
+	return data, nil
+}
