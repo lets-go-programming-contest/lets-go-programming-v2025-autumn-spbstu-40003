8940342d343d9086cb04e9edf4635b14bd232b55
Ivantsov Egor <iegorivantsovs@gmail.com>
2025-10-17 16:25:55 +0300

    [TASK-2-2]

--- ivantsov.egor/task-2-2/cmd/service/main.go
+++ ivantsov.egor/task-2-2/cmd/service/main.go
@@ -5,14 +5,17 @@ import (
 	"fmt"
 )
 
+// IntHeap — реализация min-heap для хранения чисел
 type IntHeap []int
 
-func (h IntHeap) Len() int           { return len(h) }
-func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] } // мин-куча
-func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *IntHeap) Len() int           { return len(*h) }
+func (h *IntHeap) Less(i, j int) bool { return (*h)[i] < (*h)[j] }
+func (h *IntHeap) Swap(i, j int)      { (*h)[i], (*h)[j] = (*h)[j], (*h)[i] }
 
 func (h *IntHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+	if val, ok := x.(int); ok {
+		*h = append(*h, val)
+	}
 }
 
 func (h *IntHeap) Pop() interface{} {
@@ -20,29 +23,39 @@ func (h *IntHeap) Pop() interface{} {
 	n := len(old)
 	x := old[n-1]
 	*h = old[0 : n-1]
+
 	return x
 }
 
 func main() {
-	var n, k int
-	fmt.Scan(&n)
-
-	arr := make([]int, n)
-	for i := 0; i < n; i++ {
-		fmt.Scan(&arr[i])
+	var n, kIndex int
+	if _, err := fmt.Scan(&n); err != nil {
+		return
 	}
 
-	fmt.Scan(&k)
-
-	h := &IntHeap{}
-	heap.Init(h)
-
-	for _, val := range arr {
-		heap.Push(h, val)
-		if h.Len() > k {
-			heap.Pop(h)
+	arr := make([]int, n)
+	for i := range arr {
+		if _, err := fmt.Scan(&arr[i]); err != nil {
+			return
 		}
 	}
 
-	fmt.Println((*h)[0])
+	if _, err := fmt.Scan(&kIndex); err != nil {
+		return
+	}
+
+	heapData := &IntHeap{}
+	heap.Init(heapData)
+
+	for _, v := range arr {
+		heap.Push(heapData, v)
+
+		if heapData.Len() > kIndex {
+			heap.Pop(heapData)
+		}
+	}
+
+	if heapData.Len() > 0 {
+		fmt.Println((*heapData)[0])
+	}
 }
