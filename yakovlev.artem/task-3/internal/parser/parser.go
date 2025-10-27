3ddfb56493b1bf7460ac45dd05d14fb35a25b416
nxgmvw <artem-tem00@inbox.ru>
2025-10-27 04:38:55 +0300

    [TASK-3] fix golangci-lint issues 3

--- yakovlev.artem/task-3/internal/parser/parser.go
+++ yakovlev.artem/task-3/internal/parser/parser.go
@@ -31,16 +31,15 @@ func (c *Currency) UnmarshalXML(decoder *xml.Decoder, start xml.StartElement) er
 		return fmt.Errorf("decode element: %w", err)
 	}
 
-	if num, err := parseIntFromString(rawVal.NumCode); err == nil {
-		c.NumCode = num
-	}
-	c.CharCode = strings.TrimSpace(rawVal.CharCode)
-	if nom, err := parseIntFromString(rawVal.Nominal); err == nil {
-		c.Nominal = nom
-	}
-	if val, err := parseFloatFromString(rawVal.Value); err == nil {
-		c.Value = val
-	}
+	numCode, _ := parseIntFromString(rawVal.NumCode)
+	nominal, _ := parseIntFromString(rawVal.Nominal)
+	value, _ := parseFloatFromString(rawVal.Value)
+	charCode := strings.TrimSpace(rawVal.CharCode)
+
+	c.NumCode = numCode
+	c.Nominal = nominal
+	c.Value = value
+	c.CharCode = charCode
 
 	return nil
 }
@@ -53,10 +52,13 @@ func parseFile(path string) ([]Currency, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		if os.IsNotExist(err) {
+
 			return nil, fmt.Errorf("no such file: %w", err)
 		}
+
 		return nil, fmt.Errorf("open xml: %w", err)
 	}
+
 	defer func() {
 		if cerr := file.Close(); cerr != nil {
 			panic(fmt.Errorf("close xml: %w", cerr))
@@ -69,29 +71,38 @@ func parseFile(path string) ([]Currency, error) {
 	var curs struct {
 		Values []Currency `xml:"Valute"`
 	}
+
 	if err := decoder.Decode(&curs); err != nil {
+
 		return nil, fmt.Errorf("decode xml: %w", err)
 	}
 
 	sortCurrencies(curs.Values)
+
 	return curs.Values, nil
 }
 
 func parseIntFromString(str string) (int, error) {
 	s := strings.TrimSpace(str)
+
 	number, err := strconv.Atoi(s)
 	if err != nil {
+
 		return 0, fmt.Errorf("atoi %q: %w", s, err)
 	}
+
 	return number, nil
 }
 
 func parseFloatFromString(str string) (float64, error) {
 	s := strings.TrimSpace(strings.ReplaceAll(str, ",", "."))
+
 	value, err := strconv.ParseFloat(s, 64)
 	if err != nil {
+
 		return 0, fmt.Errorf("parseFloat %q: %w", s, err)
 	}
+
 	return value, nil
 }
 
