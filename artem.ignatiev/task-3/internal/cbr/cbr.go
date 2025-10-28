a851b9d6eb075c464b8344a3a8d8d2f3af799715
Ignatiev Artem <ignatiev.ak@edu.spbstu.ru>
2025-10-28 17:56:29 +0300

    [TASK-3] rework

--- artem.ignatiev/task-3/internal/cbr/cbr.go
+++ artem.ignatiev/task-3/internal/cbr/cbr.go
@@ -2,7 +2,6 @@ package cbr
 
 import (
 	"encoding/xml"
-	"fmt"
 	"io"
 	"sort"
 	"strconv"
@@ -11,74 +10,62 @@ import (
 	"golang.org/x/net/html/charset"
 )
 
+type XMLFloat float64
+
+func (f *XMLFloat) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var s string
+	if err := d.DecodeElement(&s, &start); err != nil {
+		return err
+	}
+	s = strings.ReplaceAll(s, ",", ".")
+	v, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return err
+	}
+	*f = XMLFloat(v)
+	return nil
+}
+
+type ValuteXML struct {
+	NumCode  int      `xml:"NumCode"`
+	CharCode string   `xml:"CharCode"`
+	Nominal  int      `xml:"Nominal"`
+	ValueRaw XMLFloat `xml:"Value"`
+}
+type ValCursXML struct {
+	XMLName xml.Name    `xml:"ValCurs"`
+	Valutes []ValuteXML `xml:"Valute"`
+}
+
 type Currency struct {
 	NumCode  int     `json:"num_code"`
 	CharCode string  `json:"char_code"`
 	Value    float64 `json:"value"`
 }
 
-type valCurs struct {
-	XMLName xml.Name  `xml:"ValCurs"`
-	Valutes []valuteX `xml:"Valute"`
-}
-
-type valuteX struct {
-	NumCode  string `xml:"NumCode"`
-	CharCode string `xml:"CharCode"`
-	Nominal  string `xml:"Nominal"`
-	ValueRaw string `xml:"Value"`
-}
-
 func ParseXML(r io.Reader) ([]Currency, error) {
 	dec := xml.NewDecoder(r)
 	dec.CharsetReader = charset.NewReaderLabel
 
-	var vc valCurs
+	var vc ValCursXML
 	if err := dec.Decode(&vc); err != nil {
 		return nil, err
 	}
 
 	out := make([]Currency, 0, len(vc.Valutes))
 	for _, v := range vc.Valutes {
-		numCodeStr := strings.TrimSpace(v.NumCode)
-		charCodeStr := strings.TrimSpace(v.CharCode)
-		nominalStr := strings.TrimSpace(v.Nominal)
-		valueRawStr := strings.TrimSpace(v.ValueRaw)
-
-		if numCodeStr == "" || charCodeStr == "" || nominalStr == "" || valueRawStr == "" {
-			continue
-		}
-
-		num, err := strconv.Atoi(numCodeStr)
-		if err != nil {
-			continue
-		}
-
-		nominal, err := strconv.Atoi(nominalStr)
-		if err != nil {
-			continue
-		}
-		valStr := strings.ReplaceAll(valueRawStr, ",", ".")
-		valF, err := strconv.ParseFloat(valStr, 64)
-		if err != nil {
-			continue
-		}
-
 		var valuePerUnit float64
-		if nominal == 0 {
-			valuePerUnit = valF
+		if v.Nominal == 0 {
+			valuePerUnit = float64(v.ValueRaw)
 		} else {
-			valuePerUnit = valF / float64(nominal)
+			valuePerUnit = float64(v.ValueRaw) / float64(v.Nominal)
 		}
 
 		out = append(out, Currency{
-			NumCode:  num,
-			CharCode: charCodeStr,
+			NumCode:  v.NumCode,
+			CharCode: v.CharCode,
 			Value:    valuePerUnit,
 		})
-	}
-	if len(out) == 0 {
-		return nil, fmt.Errorf("no valid currency entries found after parsing")
 	}
 
 	sort.Slice(out, func(i, j int) bool {
