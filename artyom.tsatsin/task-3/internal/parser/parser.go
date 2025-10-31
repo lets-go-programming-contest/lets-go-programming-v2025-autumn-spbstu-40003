04480befabf21b881ae1ffccf4dc573b1e75e85d
Artem-Hack <cacinartem1@gmail.com>
2025-11-01 00:24:40 +0300

    [TASK-3] update parser line 76 for linter

--- artyom.tsatsin/task-3/internal/parser/parser.go
+++ artyom.tsatsin/task-3/internal/parser/parser.go
@@ -75,7 +75,7 @@ func (c *Currency) handleStartElement(dec *xml.Decoder, elem xml.StartElement) e
 		val, err := strconv.ParseFloat(valueStr, 64)
 
 		if err != nil {
-			return fmt.Errorf("invalid Value: %w", err)
+			return err
 		}
 
 		c.Value = val
