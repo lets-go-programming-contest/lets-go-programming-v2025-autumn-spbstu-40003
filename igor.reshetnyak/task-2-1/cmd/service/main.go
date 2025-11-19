7f7401a54b4ea2ebe2fa36312624d5c77d437edb
ReshetnyakIgor <igor.reshetnyak.99.99@mail.ru>
2025-11-19 05:19:46 +0300

    [TASK-2-1] lint fix 1

--- igor.reshetnyak/task-2-1/cmd/service/main.go
+++ igor.reshetnyak/task-2-1/cmd/service/main.go
@@ -6,48 +6,48 @@ import (
 )
 
 var (
-	ErrDepart   = errors.New("deparments error")
+	ErrDepart   = errors.New("departments error")
 	ErrEmployee = errors.New("employee error")
 	ErrTemp     = errors.New("incorrect temperature")
-	ErrSymbol   = errors.New("incorrerc symbol")
+	ErrSymbol   = errors.New("incorrect symbol")
 )
 
 func main() {
 	var departments int
 
 	if _, err := fmt.Scan(&departments); err != nil || departments < 1 || departments > 1000 {
-		fmt.Println(ErrDepart, err)
+		fmt.Println(-1)
 		return
 	}
 
-	for range departments {
+	for i := 0; i < departments; i++ {
 		var employee int
 
 		if _, err := fmt.Scan(&employee); err != nil || employee < 1 || employee > 1000 {
-			fmt.Println(ErrEmployee, err)
+			fmt.Println(-1)
 			return
 		}
 
-		deparmentOptimalTemp(employee)
+		departmentOptimalTemp(employee)
 	}
 }
 
-func deparmentOptimalTemp(employee int) {
+func departmentOptimalTemp(employee int) {
 	minTemp := 15
 	maxTemp := 30
 	inRangeTemp := true
 
-	for range employee {
+	for i := 0; i < employee; i++ {
 		var symbol string
 		var newTemp int
 
-		if _, err := fmt.Scan(&symbol); err != nil || (symbol != "<=" && symbol != ">=") {
-			fmt.Println(ErrSymbol, err)
+		if _, err := fmt.Scan(&symbol); err != nil {
+			fmt.Println(-1)
 			return
 		}
 
 		if _, err := fmt.Scan(&newTemp); err != nil {
-			fmt.Println(ErrTemp, err)
+			fmt.Println(-1)
 			return
 		}
 
