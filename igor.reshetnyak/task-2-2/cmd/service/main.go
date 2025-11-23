7ba165d233a8cbc7a57479fcab7ddba2e459efa7
ReshetnyakIgor <igor.reshetnyak.99.99@mail.ru>
2025-11-23 11:27:40 +0300

    [TASK-2-2] init commit

---
+++ igor.reshetnyak/task-2-2/cmd/service/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"container/heap"
+	"errors"
+	"fmt"
+)
+
+var (
+	ErrDishes       = errors.New("Wrong num of dishes")
+	ErrRaitingValue = errors.New("Wrong dish raiting value")
+	ErrRaitingNum   = errors.New("Wrong raiting num")
+)
+
+const (
+	minDishValue = 1
+	maxValue     = 10000
+	minValue     = -10000
+)
+
+type Heap []int
+
+func (h Heap) Len() int {
+	return len(h)
+}
+
+func (h Heap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h Heap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *Heap) Push(num interface{}) {
+	*h = append(*h, num.(int))
+}
+
+func (h *Heap) Pop() interface{} {
+	oldHeap := *h
+	oldLen := len(oldHeap)
+	num := oldHeap[oldLen-1]
+	*h = oldHeap[0 : oldLen-1]
+	return num
+}
+
+func main() {
+	var dishes, raitingNum int
+
+	if _, err := fmt.Scan(&dishes); err != nil || dishes < minDishValue || dishes > maxValue {
+		fmt.Println(ErrDishes, err)
+		return
+	}
+
+	dishRatings := make([]int, dishes)
+
+	for index := 0; index < dishes; index++ {
+		if _, err := fmt.Scan(&dishRatings[index]); err != nil || dishRatings[index] < minValue || dishRatings[index] > maxValue {
+			fmt.Println(ErrRaitingValue, err)
+			return
+		}
+	}
+
+	if _, err := fmt.Scan(&raitingNum); err != nil {
+		fmt.Println(ErrRaitingNum, err)
+		return
+	}
+
+	optimalRaiting := findOptimalRaiting(dishRatings, raitingNum)
+
+	fmt.Println(optimalRaiting)
+}
+
+func findOptimalRaiting(dishRatings []int, raitingNum int) int {
+	dishHeap := &Heap{}
+	heap.Init(dishHeap)
+
+	for _, dish := range dishRatings {
+		if dishHeap.Len() < raitingNum {
+			heap.Push(dishHeap, dish)
+		} else if dish > (*dishHeap)[0] {
+			heap.Pop(dishHeap)
+			heap.Push(dishHeap, dish)
+		}
+	}
+
+	return (*dishHeap)[0]
+}
