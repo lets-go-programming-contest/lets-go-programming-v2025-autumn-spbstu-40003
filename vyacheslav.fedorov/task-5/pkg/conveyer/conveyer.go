a50424effa796bbe90341e63036259684fc62a28
Fedorov Vyacheslav <fortochkamcs@gmail.com>
2025-12-05 18:35:32 +0300

    [TASK-5] Fixing test and linters

--- vyacheslav.fedorov/task-5/pkg/conveyer/conveyer.go
+++ vyacheslav.fedorov/task-5/pkg/conveyer/conveyer.go
@@ -14,70 +14,61 @@ var ErrStreamNotFound = errors.New("chan not found")
 const undef = "undefined"
 
 type Pipeline struct {
-	streams      map[string]chan string
 	bufferSize   int
+	streams      map[string]chan string
+	mutex        sync.RWMutex
 	decorators   []decoratorEntry
 	multiplexers []multiplexerEntry
 	separators   []separatorEntry
-	mutex        sync.Mutex
 }
 
 type decoratorEntry struct {
 	function func(ctx context.Context, input chan string, output chan string) error
-	in       string
-	out      string
+	input    string
+	output   string
 }
 
 type multiplexerEntry struct {
 	function func(ctx context.Context, inputs []chan string, output chan string) error
-	ins      []string
-	out      string
+	inputs   []string
+	output   string
 }
 
 type separatorEntry struct {
 	function func(ctx context.Context, input chan string, outputs []chan string) error
-	in       string
-	outs     []string
+	input    string
+	outputs  []string
 }
 
 func New(size int) *Pipeline {
 	return &Pipeline{
-		streams:      make(map[string]chan string),
 		bufferSize:   size,
+		streams:      make(map[string]chan string),
+		mutex:        sync.RWMutex{},
 		decorators:   make([]decoratorEntry, 0),
 		multiplexers: make([]multiplexerEntry, 0),
 		separators:   make([]separatorEntry, 0),
-		mutex:        sync.Mutex{},
 	}
 }
 
 func (p *Pipeline) Send(input string, data string) error {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
-
-	inputChannel, exists := p.streams[input]
+	ch, exists := p.getStream(input)
 	if !exists {
 		return ErrStreamNotFound
 	}
-	select {
-	case inputChannel <- 
-		return nil
-	default:
-		inputChannel <- data
-		return nil
-	}
+
+	ch <- data
+
+	return nil
 }
 
 func (p *Pipeline) Recv(output string) (string, error) {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
-
-	outputChannel, exists := p.streams[output]
+	ch, exists := p.getStream(output)
 	if !exists {
 		return "", ErrStreamNotFound
 	}
 
-	data, ok := <-outputChannel
+	data, ok := <-ch
 	if !ok {
 		return undef, nil
 	}
@@ -89,26 +80,31 @@ func (p *Pipeline) getOrCreateStream(name string) chan string {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-	if channel, exists := p.streams[name]; exists {
-		return channel
+	if ch, exists := p.streams[name]; exists {
+		return ch
 	}
 
-	channel := make(chan string, p.bufferSize)
-	p.streams[name] = channel
+	ch := make(chan string, p.bufferSize)
+	p.streams[name] = ch
 
-	return channel
+	return ch
+}
+
+func (p *Pipeline) getStream(name string) (chan string, bool) {
+	p.mutex.RLock()
+	defer p.mutex.RUnlock()
+
+	ch, exists := p.streams[name]
+
+	return ch, exists
 }
 
 func (p *Pipeline) closeAllStreams() {
 	p.mutex.Lock()
 	defer p.mutex.Unlock()
 
-	closed := make(map[chan string]bool)
 	for _, ch := range p.streams {
-		if !closed[ch] {
-			close(ch)
-			closed[ch] = true
-		}
+		close(ch)
 	}
 }
 
@@ -117,14 +113,14 @@ func (p *Pipeline) RegisterDecorator(
 	input string,
 	output string,
 ) {
-	p.decorators = append(p.decorators, decoratorEntry{
-		function: function,
-		in:       input,
-		out:      output,
-	})
-
 	p.getOrCreateStream(input)
 	p.getOrCreateStream(output)
+
+	p.decorators = append(p.decorators, decoratorEntry{
+		function: function,
+		input:    input,
+		output:   output,
+	})
 }
 
 func (p *Pipeline) RegisterMultiplexer(
@@ -132,17 +128,17 @@ func (p *Pipeline) RegisterMultiplexer(
 	inputs []string,
 	output string,
 ) {
-	p.multiplexers = append(p.multiplexers, multiplexerEntry{
-		function: function,
-		ins:      inputs,
-		out:      output,
-	})
-
-	for _, input := range inputs {
-		p.getOrCreateStream(input)
+	for _, name := range inputs {
+		p.getOrCreateStream(name)
 	}
 
 	p.getOrCreateStream(output)
+
+	p.multiplexers = append(p.multiplexers, multiplexerEntry{
+		function: function,
+		inputs:   inputs,
+		output:   output,
+	})
 }
 
 func (p *Pipeline) RegisterSeparator(
@@ -150,17 +146,17 @@ func (p *Pipeline) RegisterSeparator(
 	input string,
 	outputs []string,
 ) {
-	p.separators = append(p.separators, separatorEntry{
-		function: function,
-		in:       input,
-		outs:     outputs,
-	})
-
 	p.getOrCreateStream(input)
 
-	for _, output := range outputs {
-		p.getOrCreateStream(output)
+	for _, name := range outputs {
+		p.getOrCreateStream(name)
 	}
+
+	p.separators = append(p.separators, separatorEntry{
+		function: function,
+		input:    input,
+		outputs:  outputs,
+	})
 }
 
 func (p *Pipeline) Run(ctx context.Context) error {
@@ -168,37 +164,38 @@ func (p *Pipeline) Run(ctx context.Context) error {
 
 	workerGroup, workerGroupCtx := errgroup.WithContext(ctx)
 
-	for _, decoratorItem := range p.decorators {
-		workerGroup.Go(func() error {
-			inputChan := p.getOrCreateStream(decoratorItem.in)
-			outputChan := p.getOrCreateStream(decoratorItem.out)
+	for _, decorator := range p.decorators {
+		inputChan := p.getOrCreateStream(decorator.input)
+		outputChan := p.getOrCreateStream(decorator.output)
 
-			return decoratorItem.function(workerGroupCtx, inputChan, outputChan)
+		workerGroup.Go(func() error {
+			return decorator.function(workerGroupCtx, inputChan, outputChan)
 		})
 	}
 
-	for _, multiplexerItem := range p.multiplexers {
+	for _, multiplexer := range p.multiplexers {
+		outputChan := p.getOrCreateStream(multiplexer.output)
+		inputChannels := make([]chan string, len(multiplexer.inputs))
+
+		for index, name := range multiplexer.inputs {
+			inputChannels[index] = p.getOrCreateStream(name)
+		}
+
 		workerGroup.Go(func() error {
-			inputChannels := make([]chan string, len(multiplexerItem.ins))
-			for index, input := range multiplexerItem.ins {
-				inputChannels[index] = p.getOrCreateStream(input)
-			}
-
-			outputChan := p.getOrCreateStream(multiplexerItem.out)
-
-			return multiplexerItem.function(workerGroupCtx, inputChannels, outputChan)
+			return multiplexer.function(workerGroupCtx, inputChannels, outputChan)
 		})
 	}
 
-	for _, separatorItem := range p.separators {
-		workerGroup.Go(func() error {
-			inputChan := p.getOrCreateStream(separatorItem.in)
-			outputChannels := make([]chan string, len(separatorItem.outs))
-			for index, output := range separatorItem.outs {
-				outputChannels[index] = p.getOrCreateStream(output)
-			}
+	for _, separator := range p.separators {
+		inputChan := p.getOrCreateStream(separator.input)
+		outputChannels := make([]chan string, len(separator.outputs))
 
-			return separatorItem.function(workerGroupCtx, inputChan, outputChannels)
+		for index, name := range separator.outputs {
+			outputChannels[index] = p.getOrCreateStream(name)
+		}
+
+		workerGroup.Go(func() error {
+			return separator.function(workerGroupCtx, inputChan, outputChannels)
 		})
 	}
 
