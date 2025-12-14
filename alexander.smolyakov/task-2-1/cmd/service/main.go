9da573fdf458d88345c294f601ad859ad206b657
Ignitron1 <iamremotr@gmail.com>
2025-12-14 11:09:00 -0500

    [TASK-2-1] gofmt formatting.

--- alexander.smolyakov/task-2-1/cmd/service/main.go
+++ alexander.smolyakov/task-2-1/cmd/service/main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "os"
+	"fmt"
+	"io"
+	"os"
 
-    "github.com/Ignitron1/task-2-1/internal/conditioner"
+	"github.com/Ignitron1/task-2-1/internal/conditioner"
 )
 
 func main() {
-    var writer io.Writer = os.Stdout
-    var reader io.Reader = os.Stdin
-    
-    var departments int
-    _, error := fmt.Fscan(reader, &departments)
-    if error != nil {
-        fmt.Println("Could not read number of departments.")
-    }
 
-    for i := 0; i < departments; i++ {
-        error = conditioner.ProcessDepartment(reader, writer)
-        if error != nil {
-            fmt.Println(error)
-        }
-    }
+	var writer io.Writer = os.Stdout
+	var reader io.Reader = os.Stdin
+	var departments int
+
+	_, err := fmt.Fscan(reader, &departments)
+	if err != nil {
+		fmt.Println("Could not read number of departments.")
+	}
+
+	for i := 0; i < departments; i++ {
+		err = conditioner.ProcessDepartment(reader, writer)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 }
--- alexander.smolyakov/task-2-1/internal/conditioner/conditioner.go
+++ alexander.smolyakov/task-2-1/internal/conditioner/conditioner.go
@@ -1,122 +1,125 @@
 package conditioner
 
 import (
-    "bufio"
-    "errors"
-    "fmt"
-    "io"
-    "strconv"
-    "strings"
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
 )
 
 var (
-    errInvalidEmployeeFormat    = errors.New("invalid employee count format.")
-    errInvalidEmployeeCount     = errors.New("employee count must be greater than 0.")
-    errCommandRead              = errors.New("could not read command.")
-    errParseTemperature         = errors.New("could not parse temperature.")
-    errDataPrint                = errors.New("error printing data.")
-    errInvalidArgumentCount     = errors.New("invalid argument count.")
-    errInvalidTemperatureFormat = errors.New("invalid temperature value format.")
-    errTemperatureOutOfRange    = errors.New("temperature value out of range.")
-    errInvalidOperator          = errors.New("invalid operator.")
+	errInvalidEmployeeFormat    = errors.New("invalid employee count format")
+	errInvalidEmployeeCount     = errors.New("employee count must be greater than 0")
+	errCommandRead              = errors.New("could not read command")
+	errParseTemperature         = errors.New("could not parse temperature")
+	errDataPrint                = errors.New("error printing data")
+	errInvalidArgumentCount     = errors.New("invalid argument count")
+	errInvalidTemperatureFormat = errors.New("invalid temperature value format")
+	errTemperatureOutOfRange    = errors.New("temperature value out of range")
+	errInvalidOperator          = errors.New("invalid operator")
 )
 
 const (
-    expectedArgumentCount = 2
-    minimalTemperature    = 15
-    maximalTemperature    = 30
-    operatorGreater       = ">="
-    operatorLess          = "<="
+	expectedArgumentCount = 2
+	minimalTemperature    = 15
+	maximalTemperature    = 30
+	operatorGreater       = ">="
+	operatorLess          = "<="
 )
 
 type DepartmentProcessor struct {
-    minimalSetTemperature int
-    maximalSetTemperature int
+	minimalSetTemperature int
+	maximalSetTemperature int
 }
 
 func NewDepartmentProcessor() *DepartmentProcessor {
-    return &DepartmentProcessor{
-        minimalSetTemperature: minimalTemperature,
-        maximalSetTemperature: maximalTemperature,
-    }
+	return &DepartmentProcessor{
+		minimalSetTemperature: minimalTemperature,
+		maximalSetTemperature: maximalTemperature,
+	}
 }
 
 func (dp *DepartmentProcessor) reset() {
-    dp.minimalSetTemperature = minimalTemperature
-    dp.maximalSetTemperature = maximalTemperature
+	dp.minimalSetTemperature = minimalTemperature
+	dp.maximalSetTemperature = maximalTemperature
 }
 
 func ProcessDepartment(reader io.Reader, writer io.Writer) error {
-    processor := NewDepartmentProcessor()
+	processor := NewDepartmentProcessor()
 
-    return processor.processDepartment(reader, writer)
+	return processor.processDepartment(reader, writer)
 }
 
 func (dp *DepartmentProcessor) processDepartment(reader io.Reader, writer io.Writer) error {
-    bufReader := bufio.NewReader(reader)
+	bufReader := bufio.NewReader(reader)
+	var employees int
 
-    var employees int
-    _, err := fmt.Fscanln(reader, &employees)
-    if err != nil {
-        return errInvalidEmployeeFormat
-    }
-    if employees <= 0 {
-        return errInvalidEmployeeCount
-    }
+	_, err := fmt.Fscanln(reader, &employees)
+	if err != nil {
+		return errInvalidEmployeeFormat
+	}
 
-    dp.reset()
-    
-    for range employees {
-        command, error := bufReader.ReadString('\n')
-        if error != nil {
-            return errCommandRead
-        }
+	if employees <= 0 {
+		return errInvalidEmployeeCount
+	}
 
-        error = dp.parseTemperature(command)
-        if error != nil {
-            return errParseTemperature
-        }
+	dp.reset()
 
-        if dp.minimalSetTemperature <= dp.maximalSetTemperature {
-            _, error = fmt.Fprintln(writer, dp.minimalSetTemperature)
-            if error != nil {
-                return errDataPrint
-            }
-        } else {
-            _, error = fmt.Fprintln(writer, -1)
-            if error != nil {
-                return errDataPrint
-            }
-        }
-    }
+	for range employees {
+		var command string
+		command, err = bufReader.ReadString('\n')
+		if err != nil {
+			return errCommandRead
+		}
 
-    return nil
+		err = dp.parseTemperature(command)
+		if err != nil {
+			return errParseTemperature
+		}
+
+		if dp.minimalSetTemperature <= dp.maximalSetTemperature {
+			_, err = fmt.Fprintln(writer, dp.minimalSetTemperature)
+			if err != nil {
+				return errDataPrint
+			}
+		} else {
+			_, err = fmt.Fprintln(writer, -1)
+			if err != nil {
+				return errDataPrint
+			}
+		}
+	}
+
+	return nil
 }
 
 func (dp *DepartmentProcessor) parseTemperature(raw string) error {
-    arguments := strings.Fields(raw)
-    if len(arguments) != expectedArgumentCount {
-        return errInvalidArgumentCount
-    }
+	arguments := strings.Fields(raw)
+	if len(arguments) != expectedArgumentCount {
+		return errInvalidArgumentCount
+	}
 
-    operator := arguments[0]
-    value, err := strconv.Atoi(arguments[1])
-    if err != nil {
-        return errInvalidTemperatureFormat
-    }
+	operator := arguments[0]
 
-    if value > maximalTemperature || value < minimalTemperature {
-        return errTemperatureOutOfRange
-    }
+	value, err := strconv.Atoi(arguments[1])
+	if err != nil {
+		return errInvalidTemperatureFormat
+	}
 
-    switch operator {
-    case operatorGreater:
-        dp.minimalSetTemperature = value
-    case operatorLess:
-        dp.maximalSetTemperature = value
-    default:
-        return errInvalidOperator
-    }
+	if value > maximalTemperature || value < minimalTemperature {
+		return errTemperatureOutOfRange
+	}
 
-    return nil
+	switch operator {
+	case operatorGreater:
+		dp.minimalSetTemperature = value
+	case operatorLess:
+		dp.maximalSetTemperature = value
+	default:
+		return errInvalidOperator
+	}
+
+	return nil
 }
