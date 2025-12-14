18749a6a9cb81f305d554b93a053dcb7b3c61a95
Ignitron1 <iamremotr@gmail.com>
2025-12-14 12:17:52 -0500

    [TASK-2-1] Formatting fixes.

--- alexander.smolyakov/task-2-1/internal/conditioner/conditioner.go
+++ alexander.smolyakov/task-2-1/internal/conditioner/conditioner.go
@@ -50,22 +50,23 @@ func (dp *DepartmentProcessor) reset() {
 
 func ProcessDepartments(reader io.Reader, writer io.Writer) error {
 	processor := NewDepartmentProcessor()
+
 	return processor.processDepartments(reader, writer)
 }
 
 func (dp *DepartmentProcessor) processDepartments(reader io.Reader, writer io.Writer) error {
 	scanner := bufio.NewScanner(reader)
-	
+
 	departmentCount, err := dp.readDepartmentCount(scanner)
 	if err != nil {
 		return err
 	}
-	
+
 	allResults, err := dp.processAllDepartments(scanner, departmentCount)
 	if err != nil {
 		return err
 	}
-	
+
 	return dp.printAllResults(writer, allResults)
 }
 
@@ -73,32 +74,33 @@ func (dp *DepartmentProcessor) readDepartmentCount(scanner *bufio.Scanner) (int,
 	if !scanner.Scan() {
 		return 0, errInvalidDepartmentFormat
 	}
-	
+
 	departmentCountStr := scanner.Text()
+
 	departmentCount, err := strconv.Atoi(departmentCountStr)
 	if err != nil {
 		return 0, errInvalidDepartmentFormat
 	}
-	
+
 	if departmentCount <= 0 {
 		return 0, errInvalidDepartmentCount
 	}
-	
+
 	return departmentCount, nil
 }
 
 func (dp *DepartmentProcessor) processAllDepartments(scanner *bufio.Scanner, departmentCount int) ([][]int, error) {
 	allResults := make([][]int, 0, departmentCount)
-	
+
 	for range departmentCount {
 		departmentResults, err := dp.processSingleDepartment(scanner)
 		if err != nil {
 			return nil, err
 		}
-		
+
 		allResults = append(allResults, departmentResults)
 	}
-	
+
 	return allResults, nil
 }
 
@@ -107,19 +109,20 @@ func (dp *DepartmentProcessor) processSingleDepartment(scanner *bufio.Scanner) (
 	if err != nil {
 		return nil, err
 	}
-	
+
 	dp.reset()
+
 	departmentResults := make([]int, 0, employeeCount)
-	
+
 	for range employeeCount {
 		result, err := dp.processEmployeeCommand(scanner)
 		if err != nil {
 			return nil, err
 		}
-		
+
 		departmentResults = append(departmentResults, result)
 	}
-	
+
 	return departmentResults, nil
 }
 
@@ -127,17 +130,18 @@ func (dp *DepartmentProcessor) readEmployeeCount(scanner *bufio.Scanner) (int, e
 	if !scanner.Scan() {
 		return 0, errInvalidEmployeeFormat
 	}
-	
+
 	employeeCountStr := scanner.Text()
+
 	employeeCount, err := strconv.Atoi(employeeCountStr)
 	if err != nil {
 		return 0, errInvalidEmployeeFormat
 	}
-	
+
 	if employeeCount <= 0 {
 		return 0, errInvalidEmployeeCount
 	}
-	
+
 	return employeeCount, nil
 }
 
@@ -145,17 +149,18 @@ func (dp *DepartmentProcessor) processEmployeeCommand(scanner *bufio.Scanner) (i
 	if !scanner.Scan() {
 		return 0, errCommandRead
 	}
-	
+
 	command := scanner.Text()
+
 	err := dp.parseTemperature(command)
 	if err != nil {
 		return 0, errParseTemperature
 	}
-	
+
 	if dp.minimalSetTemperature <= dp.maximalSetTemperature {
 		return dp.minimalSetTemperature, nil
 	}
-	
+
 	return -1, nil
 }
 
@@ -168,7 +173,7 @@ func (dp *DepartmentProcessor) printAllResults(writer io.Writer, allResults [][]
 			}
 		}
 	}
-	
+
 	return nil
 }
 
