9d7fdcaa5da8364eddfdd4d8df4c919d84a82274
Alexander <alsalindr@gmail.com>
2025-12-22 16:13:17 +0300

    [TASK-6] (#148)
    * [TASK-6] Add interal/db/db.go and tests for it
    
    * [TASK-6] Rework logic in internal/db/db_test.go
    
    * [TASK-6] Add interal/wifi/wifi.go and tests for it. Add go.mod and go.sum
    
    * [TASK-6] Fix linters
    
    * [TASK-6] Fix linters 2

---
+++ alexander.vitkovsky/task-6/internal/db/db_test.go
@@ -0,0 +1,146 @@
+package db_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/alexpi3/task-6/internal/db"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	ErrRowsError  = errors.New("rows error")
+	ErrQueryError = errors.New("query error")
+)
+
+func mockDBRows(names []string) *sqlmock.Rows {
+	rows := sqlmock.NewRows([]string{"name"})
+	for _, name := range names {
+		rows = rows.AddRow(name)
+	}
+
+	return rows
+}
+
+type getNamesFunc func() ([]string, error)
+
+type getNamesTestCase struct {
+	name          string
+	rows          *sqlmock.Rows
+	expected      []string
+	expectedError bool
+	queryError    error
+}
+
+func getNamesTestCases() []getNamesTestCase {
+	return []getNamesTestCase{
+		{
+			name:     "success",
+			rows:     mockDBRows([]string{"Bob", "Steve", "Svetozar"}),
+			expected: []string{"Bob", "Steve", "Svetozar"},
+		},
+		{
+			name: "rows error",
+			rows: sqlmock.NewRows([]string{"name"}).
+				AddRow("Bob").
+				RowError(0, ErrRowsError),
+			expectedError: true,
+		},
+		{
+			name:          "scan error",
+			rows:          sqlmock.NewRows([]string{"name"}).AddRow(nil),
+			expectedError: true,
+		},
+		{
+			name:     "no rows",
+			rows:     sqlmock.NewRows([]string{"name"}),
+			expected: nil,
+		},
+		{
+			name:          "query error",
+			queryError:    ErrQueryError,
+			expectedError: true,
+		},
+	}
+}
+
+func testGetNamesMethod(
+	t *testing.T,
+	query string,
+	method func(db.DBService) getNamesFunc,
+) {
+	t.Helper()
+
+	for _, testCase := range getNamesTestCases() {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockDB, mock, err := sqlmock.New()
+			require.NoError(t, err)
+
+			service := db.DBService{DB: mockDB}
+
+			expect := mock.ExpectQuery(query)
+
+			if testCase.queryError != nil {
+				expect.WillReturnError(testCase.queryError)
+			} else {
+				expect.WillReturnRows(testCase.rows)
+			}
+
+			names, err := method(service)()
+
+			if testCase.expectedError {
+				require.Error(t, err)
+				require.Nil(t, names)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, testCase.expected, names)
+		})
+	}
+}
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	/*
+		Насколько я понял, этот тест не имеет смысла.
+		Конструктор не имеет вообще никого ветвления -> нечему ломаться.
+		Но тест есть, чтобы обеспечить покрытие 100%.
+	*/
+	mockDB, _, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	dbService := db.New(mockDB)
+
+	require.Equal(t, mockDB, dbService.DB,
+		"expected: %s, got: %s", mockDB, dbService.DB)
+}
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	testGetNamesMethod(
+		t,
+		"SELECT name FROM users",
+		func(service db.DBService) getNamesFunc {
+			return service.GetNames
+		})
+}
+
+func TestGetUniqueNames(t *testing.T) {
+	t.Parallel()
+
+	testGetNamesMethod(
+		t,
+		"SELECT DISTINCT name FROM users",
+		func(service db.DBService) getNamesFunc {
+			return service.GetUniqueNames
+		})
+}
---
+++ alexander.vitkovsky/task-6/internal/wifi/wifi_test.go
@@ -0,0 +1,151 @@
+package wifi_test
+
+import (
+	"errors"
+	"net"
+	"testing"
+
+	"github.com/mdlayher/wifi"
+	"github.com/stretchr/testify/require"
+
+	wifiPkg "github.com/alexpi3/task-6/internal/wifi"
+)
+
+var ErrSomeError = errors.New("some error")
+
+type mockWifi struct {
+	interfaces []*wifi.Interface
+	err        error
+}
+
+func (m *mockWifi) Interfaces() ([]*wifi.Interface, error) {
+	return m.interfaces, m.err
+}
+
+func TestNew(t *testing.T) {
+	t.Parallel()
+
+	mockWifi := &mockWifi{}
+
+	service := wifiPkg.New(mockWifi)
+
+	require.Equal(t, mockWifi, service.WiFi)
+}
+
+func TestGetNames(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name        string
+		interfaces  []*wifi.Interface
+		err         error
+		expected    []string
+		expectedErr bool
+	}{
+		{
+			name: "success",
+			interfaces: []*wifi.Interface{
+				{Name: "wifi0"},
+				{Name: "wifi1"},
+				{Name: "wifi2"},
+			},
+			expected: []string{"wifi0", "wifi1", "wifi2"},
+		},
+		{
+			name:        "interfaces error",
+			err:         ErrSomeError,
+			expectedErr: true,
+		},
+		{
+			name:       "no interfaces",
+			interfaces: []*wifi.Interface{},
+			expected:   []string{},
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockWifi := &mockWifi{
+				interfaces: testCase.interfaces,
+				err:        testCase.err,
+			}
+
+			service := wifiPkg.New(mockWifi)
+
+			names, err := service.GetNames()
+
+			if testCase.expectedErr {
+				require.Error(t, err)
+				require.Nil(t, names)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, testCase.expected, names)
+		})
+	}
+}
+
+func TestGetAddresses(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name        string
+		interfaces  []*wifi.Interface
+		err         error
+		expected    []net.HardwareAddr
+		expectedErr bool
+	}{
+		{
+			name: "success",
+			interfaces: []*wifi.Interface{
+				{Name: "wifi0", HardwareAddr: net.HardwareAddr{0x1, 0x2, 0x3, 0x4, 0x5, 0x6}},
+				{Name: "wifi1", HardwareAddr: net.HardwareAddr{0xaa, 0xbb, 0xcc}},
+				{Name: "wifi2", HardwareAddr: net.HardwareAddr{0xdd, 0xee, 0xff}},
+			},
+			expected: []net.HardwareAddr{
+				{0x1, 0x2, 0x3, 0x4, 0x5, 0x6},
+				{0xaa, 0xbb, 0xcc},
+				{0xdd, 0xee, 0xff},
+			},
+		},
+		{
+			name:        "interfaces error",
+			err:         ErrSomeError,
+			expectedErr: true,
+		},
+		{
+			name:       "no interfaces",
+			interfaces: []*wifi.Interface{},
+			expected:   []net.HardwareAddr{},
+		},
+	}
+
+	for _, testCase := range cases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			mockWifi := &mockWifi{
+				interfaces: testCase.interfaces,
+				err:        testCase.err,
+			}
+
+			service := wifiPkg.New(mockWifi)
+
+			addresses, err := service.GetAddresses()
+
+			if testCase.expectedErr {
+				require.Error(t, err)
+				require.Nil(t, addresses)
+
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, testCase.expected, addresses)
+		})
+	}
+}
