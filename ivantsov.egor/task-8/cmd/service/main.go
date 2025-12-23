4544b208e3f823034d67b02c64b7267e10cf09d9
Ivantsov Egor <iegorivantsovs@gmail.com>
2025-12-23 17:53:45 +0300

    [TASK-8] Fix go.mod after rebase

--- ivantsov.egor/task-8/cmd/service/main.go
+++ ivantsov.egor/task-8/cmd/service/main.go
@@ -4,11 +4,7 @@ import (
 	"fmt"
 	"log"
 
-<<<<<<< HEAD
-	"github.com/lets-go-programming-contest/lets-go-programming-v2025-autumn-spbstu-40003/ivantsov.egor/task-8/internal/config"
-=======
 	"github.com/Egor1726/lets-go-programming-contest/ivantsov.egor/task-8/internal/config"
->>>>>>> 86a1198 ([TASK-8] Initial config structure)
 )
 
 func main() {
--- ivantsov.egor/task-8/go.mod
+++ ivantsov.egor/task-8/go.mod
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-module github.com/lets-go-programming-contest/lets-go-programming-v2025-autumn-spbstu-40003/ivantsov.egor/task-8
-=======
 module github.com/Egor1726/lets-go-programming-contest/ivantsov.egor/task-8
->>>>>>> 86a1198 ([TASK-8] Initial config structure)
 
 go 1.22.7
 
