ba527fc92eb3e9bd9c23b5b36cc2cf527c3703e1
ReshetnyakIgor <94829756+ReshetnyakIgor@users.noreply.github.com>
2026-01-08 18:07:38 +0300

    [TASK-3] (#161)
    * [TASK-3] init commit
    
    * [TASK-3] lint fix

---
+++ igor.reshetnyak/task-3/internal/processor/processor.go
@@ -0,0 +1,76 @@
+package processor
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/ReshetnyakIgor/task-3/internal/currency"
+	"golang.org/x/net/html/charset"
+)
+
+func LoadExchangeData(path string) (*currency.ExchangeData, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open XML file: %w", err)
+	}
+
+	defer func() {
+		if cerr := file.Close(); cerr != nil {
+			fmt.Println("close error:", cerr)
+		}
+	}()
+
+	decoder := xml.NewDecoder(file)
+	decoder.CharsetReader = charset.NewReaderLabel
+
+	var exchangeData currency.ExchangeData
+	if err := decoder.Decode(&exchangeData); err != nil {
+		return nil, fmt.Errorf("XML decoding error: %w", err)
+	}
+
+	return &exchangeData, nil
+}
+
+func GetSortedCurrencies(data *currency.ExchangeData) []currency.Item {
+	items := make([]currency.Item, len(data.Items))
+	copy(items, data.Items)
+
+	sort.SliceStable(items, func(i, j int) bool {
+		return items[i].RateValue > items[j].RateValue
+	})
+
+	return items
+}
+
+func ExportToJSON(path string, data []currency.Item) error {
+	const filePerm = 0o755
+
+	dir := filepath.Dir(path)
+	if err := os.MkdirAll(dir, filePerm); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	file, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create JSON file: %w", err)
+	}
+
+	defer func() {
+		if cerr := file.Close(); cerr != nil {
+			fmt.Println("close error:", cerr)
+		}
+	}()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+
+	if err := encoder.Encode(data); err != nil {
+		return fmt.Errorf("JSON encoding error: %w", err)
+	}
+
+	return nil
+}
